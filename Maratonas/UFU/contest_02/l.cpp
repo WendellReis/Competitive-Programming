7fd5f402690c0d7bb73854d5a70da7fec952d08a
WendellReis <wendellreismatias@gmail.com>
2023-03-25 11:30:22 -0300

    l.cpp corrigido

--- Maratonas/UFU/contest_02/l.cpp
+++ Maratonas/UFU/contest_02/l.cpp
@@ -2,28 +2,83 @@
 
 using namespace std;
 
-int main() {
+struct seq
+{
+    int n;
+    int f;
+};
+
+int main()
+{
 
     int N;
     cin >> N;
-    int numeros[N] = {0};
+    int aN = 0;
+    int rN = 0;
+    int seqA = 0;
+    int sequencia = 0;
+    vector<seq> numeros;
 
     for (int i = 0; i < N; i++)
     {
         int n;
         cin >> n;
-        numeros[n]++;
+        if (i == 0)
+            rN = n;
+        if (rN != n)
+        {
+            if (sequencia > seqA) {
+                seqA = sequencia;
+            }
+            int found =0;
+            for (int c = 0; c < numeros.size(); c++)
+            {
+                if(numeros[c].n == rN) {
+                    found = 1;
+                    if( numeros[c].f < sequencia)
+                        numeros[c].f = sequencia;
+                    break;
+                }
+            }
+            if(!found) {
+                seq F;
+                F.f = sequencia;
+                F.n = rN;
+                numeros.push_back(F);
+            }
+            rN = n;
+            sequencia = 1;
+        }
+        else
+        {
+            rN = n;
+            sequencia++;
+        }
+        if(i == N-1) {
+            int found =0;
+            for (int c = 0; c < numeros.size(); c++)
+            {
+                if(numeros[c].n == rN) {
+                    found = 1;
+                    if( numeros[c].f < sequencia)
+                        numeros[c].f = sequencia;
+                    break;
+                }
+            }
+            if(!found) {
+                seq F;
+                F.f = sequencia;
+                F.n = rN;
+                numeros.push_back(F);
+            }
+        }
     }
 
-    int maior = 0;
-    for (int i = 1; i < N; i++)
+    for (int i = 0; i < numeros.size(); i++)
     {
-        if(numeros[i] > numeros[maior])
-            maior = i;
+        if (numeros[i].f == seqA)
+            cout << numeros[i].n << " ";
     }
-
-    cout << maior << endl;
-
 
     return 0;
 }
