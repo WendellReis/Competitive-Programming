ad466f381e0ca1dea93d75c5e8fcdedf15f0f5d5
WendellReis <wendellreismatias@gmail.com>
2023-03-31 21:46:31 -0300

    Correçao da função binary de face.cpp e modificação em divisao.cpp

--- Maratonas/UFU/2022/contest_03/divisao.cpp
+++ Maratonas/UFU/2022/contest_03/divisao.cpp
@@ -3,13 +3,10 @@
 using namespace std;
 
 int main(){
-    int c,l, tam;
+    int c,l, tam = 1;
     cin >> c >> l;
-    for(int i = 1; i*i <= c*l; i++){
-        if((c*l)%(i*i) == 0) 
-            tam = (c*l)/(i*i);  
-    }
-
+    for(int i = 1; i*i <= c*l; i++)
+        if(c%i == 0 && l%i == 0) tam = (c*l)/(i*i);
     cout << tam << endl;
     return 0;
 }
