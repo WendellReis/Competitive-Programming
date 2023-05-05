ed18a7c1e42a6b038058a60d3b98261915e400c8
WendellReis <wendellreismatias@gmail.com>
2023-05-04 23:00:45 -0300

    Exercicio da Bruxa

--- beecrowd/VI Maratona Mineira de Programacao/bruxa.cpp
+++ beecrowd/VI Maratona Mineira de Programacao/bruxa.cpp
@@ -2,20 +2,27 @@
 
 using namespace std;
 
+int inteiro(string s);
+
 int main(){
-    while(1){
-    int a, b, r;
+    string a, b;
     char c;
-
     cin >> a >> c >> b;
-    if(c == '+') r = a + b;
-    else if (c == 'x') r = a*b;
-    else if (c == '-') r = a + b;
-    else r = a/b;
+    int r = 0;
+    if(c == '+') r = inteiro(a) + inteiro(b); 
+    else if(c == 'x') r = inteiro(a) * inteiro(b);
+    else if(c == '-') r = inteiro(a) - inteiro(b);
+    else if(inteiro(b) != 0) r = inteiro(a) / inteiro(b);
 
-    if(r%7) cout << r;
-    else cout << 0;
-    cout << endl;
-    }
+    string res = to_string(r);
+    cout << inteiro(res) << endl;
     return 0;
+}
+
+int inteiro(string s){
+    int num = 0, tam = s.size() - 1;
+    for(int i = tam; i >= 0; i--)
+        if(s[i] != '7') 
+            num = num + (int)(s[i] - '0')*pow(10,tam - i);
+    return num;
 }
