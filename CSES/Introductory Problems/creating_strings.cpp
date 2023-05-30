a9e108d411542e5eaffa4f1637898959224d2f69
WendellReis <wendellreismatias@gmail.com>
2023-05-30 18:20:22 -0300

    Creating Strings Exercicise

--- CSES/Introductory Problems/creating_strings.cpp
+++ CSES/Introductory Problems/creating_strings.cpp
@@ -1,46 +1,41 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-void solve(vector<char> palavra, vector<char> disp, vector<vector<char>>& lista){
-    if(disp.size() == 1){
-        lista.push_back(palavra);
-    } else{
-        cout << disp.size();
-        char ant = disp[0];
-        int i = 0;
-        while(!disp.empty()){
-            i++;
-            if(disp[i] != ant || i+1 == disp.size()){
-                char temp = disp[i];
-                disp.erase(disp.begin());
-                palavra.push_back(ant);
-                solve(palavra,disp, lista);
-                i = 0;
-                ant = temp;
-            }
-        }
-    }
-    
-}
-
+ 
+void createString(string s, vector<char> vet, vector<vector<char>> *mem);
+ 
 int main(){
-    string s;
-    cin >> s;
-    vector<char> cadeia, palavra;
-
-    for(int i = 0; i < s.length(); i++)
-        cadeia.push_back(s[i]);
-    sort(cadeia.begin(), cadeia.end());
+    string l; cin >> l;
+    vector<char> vet; 
+    vector<vector<char>> mem;
     
-    vector<vector<char>> lista;
-    solve(palavra, cadeia, lista);
-    cout << lista.size() << "\n";
-    for(int i = 0; i < lista.size(); i++){
-        for(int j = 0; j < lista[i].size(); j++)
-            cout << lista[i][j];
+    sort(l.begin(), l.end());
+    createString(l,vet,&mem);
+ 
+    cout << mem.size() << endl;
+    for(int i = 0; i < (int)mem.size(); i++){
+        for(int j = 0; j < (int)mem[i].size(); j++)
+            cout << mem[i][j];
         cout << "\n";
     }
+ 
     return 0;
-
+}
+ 
+void createString(string s, vector<char> vet, vector<vector<char>>* mem){
+    if(s.size() == 0)
+        mem->push_back(vet);
+ 
+    char prev = '!';
+    for(int i = 0; i < (int)s.size(); i++){
+        string s2 = s;
+        char c = s[i];
+        if(c == prev)
+            continue;
+        vet.push_back(c);
+        s2.erase(s2.begin() + i);
+        createString(s2,vet,mem);
+        vet.erase(vet.begin() + vet.size() - 1);
+        prev = c;
+    }
 }
