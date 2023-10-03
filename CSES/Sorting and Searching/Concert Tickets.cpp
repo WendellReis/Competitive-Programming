3c6d4b09a3a0d9bde4a11c12cfdf2b7934af9a8c
WendellReis <wendellreismatias@gmail.com>
2023-10-03 18:21:26 -0300

    Update

--- CSES/Sorting and Searching/Concert Tickets.cpp
+++ CSES/Sorting and Searching/Concert Tickets.cpp
@@ -18,13 +18,12 @@ int main(){
     
     for(int i = 0; i < m; i++){
         cin >> t[i];
-
-        //t[i]+1 pois t[i] é um valor dentro do limite de busca
+        
         auto it = tickets.lower_bound({t[i]+1,0});
         if(it == tickets.begin())
             cout << "-1\n";
         else{
-            it--; //lower_bound retornar o rimeiro elemento que não é menor do que o valor passado como argumento
+            it--;
             cout << (*it).first << "\n";
             tickets.erase(it);
         }
