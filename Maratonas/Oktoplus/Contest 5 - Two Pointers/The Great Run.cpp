43f8a7be6f936f5cc12992fd98979584bd8a9f42
WendellReis <wendellreismatias@gmail.com>
2023-11-07 10:23:03 -0300

    The Great Run

--- Maratonas/Oktoplus/Contest 5 - Two Pointers/The Great Run.cpp
+++ Maratonas/Oktoplus/Contest 5 - Two Pointers/The Great Run.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main(){
-    int t,n; cin >> t;
+    int t,n,k; cin >> t;
 
     while(t > 0){
-        cin >> n;
+        cin >> n >> k;
 
-        vector<int> v(n);
-        for(int i = 0; i < n; i++)
-            cin >> v[i];
-        
-        int maior = 1, menor = n, count = 1;
-        for(int i = 1; i < n; i++){
-            if(v[i]-v[i-1] > 2){
-                maior = max(maior,count);
-                menor = min(menor,count);
-                count = 1;
-            } else
-                count++;
+        vector<int> prefix(n+1); prefix[0] = 0;
+        for(int i = 1; i <= n; i++){
+            cin >> prefix[i];
+            prefix[i]+=prefix[i-1];
         }
-        cout << min(menor,count) << " " << max(maior,count)<< "\n";
+
+        if(k >= n)
+            cout << prefix[n] << "\n";
+        else{
+            int max_g = 0;
+            for(int i = k,j = 0; i <= n; i++, j++)
+                max_g = max(max_g,prefix[i]-prefix[j]);
+            cout << max_g << "\n";
+        }
+        
         t--;
     }
     return 0;
