0250e7987c5ee8e4487402b2a1b3394b2c28c685
WendellReis <wendellreismatias@gmail.com>
2023-11-17 18:07:39 -0300

    Sum of Four Values

---
+++ CSES/Sorting and Searching/Sum of Four Values.cpp
@@ -0,0 +1,47 @@
+/*
+    Problem Name: Sum of Four Values
+    Problem Link: https://cses.fi/problemset/task/1642/
+    Author: Wendell R. M. Matias (Foxtrotbr)
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define F first
+#define S second
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n,x,v; cin >> n >> x;
+
+    vector<pair<int,int>> values;
+    for(int i = 1; i <= n; i++){
+        cin >> v;
+        if(v < x-2)
+            values.push_back({v,i});
+    }
+
+    sort(values.begin(),values.end());
+    for(int i = 0; i < (int)values.size(); i++)
+        for(int j = i+1; j < (int)values.size(); j++){
+            int sum = values[i].F + values[j].F;
+            int l = j+1, r = values.size()-1;
+            
+            while(l < r){
+                int tot = sum + values[l].F + values[r].F;
+                if(tot < x)
+                    l++;
+                else if(tot > x)
+                    r--;
+                else{
+                    cout << values[i].S << " " << values[j].S << " " << values[l].S << " " << values[r].S << "\n";
+                    return 0;
+                }
+            }
+        }
+
+    cout << "IMPOSSIBLE\n";
+    return 0;
+}
