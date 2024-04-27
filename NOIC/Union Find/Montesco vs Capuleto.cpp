6f88a9375dfa63874c3b8e5f410264c5050193d0
WendellReis <wendellreismatias@gmail.com>
2024-04-27 13:14:16 -0300

    War

---
+++ NOIC/Union Find/Montesco vs Capuleto.cpp
@@ -0,0 +1,10 @@
+//Problem Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1446
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
--- NOIC/Union Find/War.cpp
+++ NOIC/Union Find/War.cpp
@@ -1,28 +1,53 @@
-//Problem Link: https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1099
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define TAM 1000
+int amigo[10001], inimigo[10001];
 
-int pai[TAM+1], peso[TAM+1] = {0}, inimigo[TAM+1] = {0};
-
-int find(int x) {
-    if(pai[x] == x) return x;
-    return pai[x] = find(pai[x]);
+int findAmigo(int x) {
+    if(x == amigo[x]) return x;
+    return amigo[x] = findAmigo(amigo[x]);
 }
 
-void join(int x, int y) {
-
+int findInimigo(int x) {
+    if(x == inimigo[x]) return x;
+    return inimigo[x] = findInimigo(inimigo[x]);
 }
 
 int main() {
     int n; cin >> n;
 
-    for(int i = 1; i <= n; i++)
-        pai[i] = i;
+    for(int i = 0; i < n; i++)
+        amigo[i] = i, inimigo[i] = n+i;
     
+    int c,a,b; cin >> c >> a >> b;
+    cout << amigo[a] << findAmigo(a);
+    while(c != 0 || a != 0 || b != 0) {
+        int a1 = findAmigo(a), a2 = findInimigo(a);
+        int b1 = findAmigo(b), b2 = findInimigo(b);
+        
+        if(c == 1) {
+            if(a1 == b2) cout << "-1\n";
+            else {
+                amigo[a1] = amigo[b1];
+                inimigo[a2] = inimigo[b2];
+            }
+        } else if(c == 2) {
+            if(a1 == b1) cout << "-1\n";
+            else {
+                amigo[a1] = amigo[b2];
+                inimigo[a2] = inimigo[b1];
+            }
+        } else if(c == 3) {
+            if(a1 == b1) cout << "1\n";
+            else cout << "0\n";
+        } else if(c == 4) {
+            if(a1 == b2) cout << "1\n";
+            else cout << "0\n";
+        }
+
+        cin >> c >> a >> b;
+    }
 
     return 0;
 }
